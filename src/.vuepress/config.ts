28d7e4b86f734ebdfe60832f25620fa9ac024d18
cyoukon <z1162289133@gmail.com>
2024-12-11 19:51:45 +0800

    跟路由

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/vuepress-starter/",
+  base: "/Notes/",
   
   host: '127.0.0.1',
   port: 8080,
